e6729c6f388b5e2fded324e651d19150d10f5f3c
Enfield-Li <88321505+Enfield1234@users.noreply.github.com>
2022-04-29 23:27:34 +0800

    half way

--- App/src/App.java
+++ App/src/App.java
@@ -1,24 +1,58 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 public class App {
 
   public static void main(String[] args) {
-    String name = JOptionPane.showInputDialog("Enter your name");
-    Integer age = Integer.parseInt(
-      JOptionPane.showInputDialog("Enter your age")
+    // ** Array **
+    // Fixed length hold primitives or objects
+    String[] arr = new String[4];
+    String[] arr2 = { "arr0", "arr1" };
+    List<String> list = Arrays.asList("list1", "list2");
+
+    // ** List **
+    List<String> list2 = new ArrayList<>(
+      Arrays.asList("11", "22") // mutable list
     );
-    Float height = Float.parseFloat(
-      JOptionPane.showInputDialog("Enter your height")
+    list2.add("e");
+
+    // ** ArrayList **
+    // Not fixed length hold only objects(Wrapper class) not primitives
+    ArrayList<String> arrList = new ArrayList<>();
+    ArrayList<String> arrList1 = new ArrayList<>(
+      Arrays.asList("al1", "al2") // mutable list
     );
 
-    JOptionPane.showMessageDialog(
-      null,
-      "hello " +
-      name +
-      " and you are " +
-      age +
-      " years old and you are " +
-      height
-    );
+    // ** Operations **
+    // access
+    System.out.println(arr[0] + arr.length);
+    System.out.println(arrList1.get(1) + arrList1.size());
+    System.out.println(list.get(0));
+
+    // add
+    arrList1.add("ar3");
+
+    // update
+    arr2[0] = "1";
+    arrList1.set(0, "ar0");
+    list.set(0, "update 0 index");
+
+    // delete
+    arrList1.remove(0);
+
+    System.out.println(list); // print raw element
+    System.out.println(arr2); // print memory
+    System.out.println(arrList1); // print element with toString/loop
+    // ArrayList<String> arrList2 = Arrays.asList("akldf", "skzdf"); // Immutable list
+
+    // ** 2D ArrayList **
+    List<String> twoDList = new ArrayList<>();
+    twoDList.add("one");
+    twoDList.add("two");
+    twoDList.add("three");
+
+    System.out.println(twoDList);
   }
 }
