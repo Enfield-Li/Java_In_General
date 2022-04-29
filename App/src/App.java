5bcb591ddc85a3d1264af116186c1dd37c3106a7
Enfield-Li <88321505+Enfield1234@users.noreply.github.com>
2022-04-30 00:37:37 +0800

    done

--- App/src/App.java
+++ App/src/App.java
@@ -3,14 +3,6 @@ import java.util.Random;
 public class App {
 
   public static void main(String[] args) {
-    Random random = new Random();
-
-    int x = random.nextInt(2);
-    double y = random.nextDouble();
-    boolean z = random.nextBoolean();
-
-    System.out.println(x);
-    System.out.println(y);
-    System.out.println(z);
+    System.out.println("hello");
   }
 }
