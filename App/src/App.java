46c12c193501e26131a2480a42d21ada7f21db9a
Enfield-Li <1076462153@qq.com>
2022-04-29 16:01:24 +0800

    Throw error

--- App/src/App.java
+++ App/src/App.java
@@ -1,7 +1,10 @@
+import exceptions.Exceptions;
+
 public class App {
 
   public static void main(String[] args) {
     System.out.println("aldkf");
+    Exceptions.show();
     System.exit(0);
   }
 }
---
+++ App/src/exceptions/Exceptions.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class Exceptions {
+
+  public static void show() {
+    sayHello(null);
+  }
+
+  public static void sayHello(String name) {
+    System.out.println(name.toUpperCase());
+  }
+}
