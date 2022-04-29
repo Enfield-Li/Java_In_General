762600b78aecfa2a3eb2326835fd7fcd60db43da
Enfield-Li <1076462153@qq.com>
2022-04-29 17:00:07 +0800

    Done

--- App/src/App.java
+++ App/src/App.java
@@ -1,7 +1,24 @@
+import javax.swing.JOptionPane;
+
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("aldkf");
-    System.exit(0);
+    String name = JOptionPane.showInputDialog("Enter your name");
+    Integer age = Integer.parseInt(
+      JOptionPane.showInputDialog("Enter your age")
+    );
+    Float height = Float.parseFloat(
+      JOptionPane.showInputDialog("Enter your height")
+    );
+
+    JOptionPane.showMessageDialog(
+      null,
+      "hello " +
+      name +
+      " and you are " +
+      age +
+      " years old and you are " +
+      height
+    );
   }
 }
