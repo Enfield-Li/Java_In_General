4e9a3a6190f8a2242cab5d7191b7d10888f9ec91
Enfield-Li <1076462153@qq.com>
2022-04-29 16:44:24 +0800

    Done

--- App/src/App.java
+++ App/src/App.java
@@ -3,8 +3,16 @@ import exceptions.Exceptions;
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("aldkf");
-    Exceptions.show();
+    
+    
+    try {
+      Exceptions.show();
+    } catch (NullPointerException e) {
+      System.out.println("error below");
+      System.out.println(e);
+    } catch (Exception e) {
+      System.out.println("wrong");
+    }
     System.exit(0);
   }
 }
--- App/src/exceptions/Exceptions.java
+++ App/src/exceptions/Exceptions.java
@@ -1,9 +1,38 @@
 package exceptions;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class Exceptions {
 
   public static void show() {
-    sayHello(null);
+    FileReader reader = null;
+
+    try {
+      reader = new FileReader("new one");
+      System.out.println("File found here");
+      new SimpleDateFormat().parse("");
+    } catch (IOException | ParseException e) {
+      IOException ex = new FileNotFoundException("no such file");
+
+      System.out.println(ex.getMessage());
+
+      e.printStackTrace();
+    } finally {
+      System.out.println("Starts final");
+      if (reader != null) {
+        try {
+          reader.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      System.out.println("That's all");
+    }
+    // sayHello(null);
   }
 
   public static void sayHello(String name) {
