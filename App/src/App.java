03d051b286f2a53b217df4bcf8bdecfcbbcd0659
Enfield-Li <1076462153@qq.com>
2022-04-29 17:10:42 +0800

    Done

--- App/src/App.java
+++ App/src/App.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("aldkf");
-    System.exit(0);
+    double x = 3.14;
+    double y = -10;
+    double z = Math.max(x, y);
+    // min, abs, sqrt, round, ceil, floor
+    // System.out.println(z);
+
+    double side1;
+    double side2;
+    double side3;
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter side 1: ");
+    side1 = scanner.nextDouble();
+
+    System.out.println("Enter side 2: ");
+    side2 = scanner.nextDouble();
+
+    side3 = Math.sqrt((side1 * side1) + (side2 * side2));
+
+    System.out.println("side3 is: " + side3);
+    scanner.close();
   }
 }
