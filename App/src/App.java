2c55ea378865ff950afc1c918213d07ab0d22865
Enfield-Li <88321505+Enfield1234@users.noreply.github.com>
2022-04-30 13:48:37 +0800

    done

--- App/src/App.java
+++ App/src/App.java
@@ -1,6 +1,39 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("hello");
+    File file = new File("App\\src\\test.txt");
+
+    if (file.exists()) {
+      System.out.println("exist");
+    } else {
+      System.out.println("doesn't exist");
+    }
+
+    try {
+      FileWriter writter = new FileWriter("App\\src\\test.txt");
+      writter.write("new stuff");
+      // writter.append();
+      writter.close();
+    } catch (Exception e) {
+      System.out.println("catch no writter file");
+    }
+
+    try {
+      FileReader reader = new FileReader("App\\src\\test.txt");
+      int data = reader.read();
+
+      while (data != -1) {
+        System.out.print((char) data);
+        data = reader.read();
+      }
+
+      reader.close();
+    } catch (Exception e) {
+      System.out.println("catch no reader file");
+    }
   }
 }
