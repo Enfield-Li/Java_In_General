ec0309eb764d7faeb89d0b4f2df9c74e98dbb9c6
Enfield-Li <1076462153@qq.com>
2022-04-30 18:56:41 +0800

    Done

--- App/src/App.java
+++ App/src/App.java
@@ -5,20 +5,18 @@ import javax.swing.JFrame;
 public class App {
 
   public static void main(String[] args) {
-    // JFrame frame = new JFrame(); // create a frame
-    // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    // frame.setTitle("JFrame title");
-    // frame.setResizable(false);
-    // frame.setVisible(true); // visiblity
-    // frame.setSize(420, 420); // size
+    Integer[] intArr = { 1, 2, 3 };
+    String[] strArr = { "1", "2", "3" };
+    Double[] dbArr = { 1.0, 2.0, 3.0 };
 
-    // // frame.getContentPane().setBackground(Color.GREEN);
-    // // frame.getContentPane().setBackground(new Color(0, 0, 0));
-    // frame.getContentPane().setBackground(new Color(0x12345));
-    // // Change Icon
-    // // ImageIcon image = new ImageIcon("xxx.png"); // create an imageIcon
-    // // frame.setIconImage(image.getImage());
+    displayArr(intArr);
+    displayArr(strArr);
+    displayArr(dbArr);
+  }
 
-    new Myframe();
+  public static <T> void displayArr(T[] arr) {
+    for (T x : arr) {
+      System.out.println(x);
+    }
   }
 }
