6c61c09d500dc10845b722c338944aa27aee56d4
Enfield-Li <88321505+Enfield1234@users.noreply.github.com>
2022-04-30 11:32:57 +0800

    .

--- App/src/App.java
+++ App/src/App.java
@@ -3,32 +3,47 @@ import java.util.Random;
 public class App {
 
   public static void main(String[] args) {
-    UserRO res = genUserInfoRO("username", "email");
-    UserRO res2 = genUserErrorRO("fieldOne", "msg1");
+    UserRO userRO = new UserRO();
+    UserRO userRO2 = new UserRO();
 
+    UserRO res = genUserInfoRO(userRO, "username", "email");
+    UserRO res2 = genUserErrorRO(userRO2, "fieldOne", "msg1");
+
+    System.out.println(res.error);
     System.out.println(res2.toString());
-    System.out.println(res.toString());
+
+    UserRO userRO3 = new UserRO();
+    UserRO.UserError error = new UserRO.UserError();
+    error.field = "fiel";
+    error.message = "adsfa";
+    userRO3.error = error;
+    // userRO3.setError(error);
+    System.out.println(userRO3.error);
   }
 
-  private static UserRO genUserInfoRO(String username, String email) {
-    UserRO res = new UserRO();
-
+  private static UserRO genUserInfoRO(
+    UserRO userRO,
+    String username,
+    String email
+  ) {
     UserRO.User user = new UserRO.User();
     user.username = username;
     user.email = email;
 
-    res.setUser(user);
-    return res;
+    userRO.setUser(user);
+    return userRO;
   }
 
-  private static UserRO genUserErrorRO(String field, String message) {
-    UserRO res = new UserRO();
-
+  private static UserRO genUserErrorRO(
+    UserRO userRO,
+    String field,
+    String message
+  ) {
     UserRO.UserError error = new UserRO.UserError();
     error.field = field;
     error.message = message;
 
-    res.setError(error);
-    return res;
+    userRO.setError(error);
+    return userRO;
   }
 }
