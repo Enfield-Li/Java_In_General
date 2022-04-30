a0e22bca7b6009bf3ca7368bb63532d96a2cf17e
Enfield-Li <1076462153@qq.com>
2022-04-30 18:35:53 +0800

    .

--- App/src/App.java
+++ App/src/App.java
@@ -1,7 +1,23 @@
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("aldkf");
-    System.exit(0);
+    JFrame frame = new JFrame(); // create a frame
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setTitle("JFrame title");
+    frame.setResizable(false);
+    frame.setVisible(true); // visiblity
+    frame.setSize(420, 420); // size
+
+    // frame.getContentPane().setBackground(Color.GREEN);
+    // frame.getContentPane().setBackground(new Color(0, 0, 0));
+    frame.getContentPane().setBackground(new Color(0x12345));
+    // Change Icon
+    // ImageIcon image = new ImageIcon("xxx.png"); // create an imageIcon
+    // frame.setIconImage(image.getImage());
+
   }
 }
