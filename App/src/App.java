447cc426f6ea73c8a28837bbacf2324bf40d2216
Enfield-Li <1076462153@qq.com>
2022-04-30 18:24:12 +0800

    done

--- App/src/App.java
+++ App/src/App.java
@@ -1,7 +1,24 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import javax.sound.sampled.*;
+
 public class App {
 
-  public static void main(String[] args) {
-    System.out.println("aldkf");
-    System.exit(0);
+  public static void main(String[] args)
+    throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+    File file = new File("src\\sound.wav");
+
+    AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+
+    Clip clip = AudioSystem.getClip();
+
+    clip.open(audioStream);
+
+    clip.start();
+
+    Scanner scanner = new Scanner(System.in);
+
+    String res = scanner.next();
   }
 }
