e9e342947fe342df933a65f4ad17cae1881eaf66
Enfield-Li <88321505+Enfield1234@users.noreply.github.com>
2022-05-01 13:26:27 +0800

    .

--- App/src/App.java
+++ App/src/App.java
@@ -8,17 +8,15 @@ public class App {
     hMap.put("personOne".hashCode(), "name1");
     hMap.putIfAbsent("personThree".hashCode(), "name3");
     hMap.put("personTwo".hashCode(), "name2");
-
-    // hMap.putIfAbsent("personTwo", "name222");
-
     hMap.replace("personTwo".hashCode(), "name222");
     // hMap.remove("personThree");
+    // hMap.putIfAbsent("personTwo", "name222");
 
     // System.out.println(hMap);
     // System.out.println(hMap.get("personOne".hashCode()));
     // System.out.println(hMap.containsKey("personTwo1".hashCode()));
     // System.out.println(hMap.containsValue("name222"));
-    
+
     // for (Integer i : hMap.keySet()) {
     //   System.out.println(i);
     //   System.out.println(hMap.get(i));
