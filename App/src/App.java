91188bc849bac94bfe99bcd18bc9e57dc8f97071
Enfield-Li <1076462153@qq.com>
2022-05-09 00:53:31 +0800

    Done

--- App/src/App.java
+++ App/src/App.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import javax.lang.model.element.NestingKind;
 import javax.swing.JOptionPane;
 
 public class App {
@@ -17,6 +22,7 @@ public class App {
       Arrays.asList("11", "22") // mutable list
     );
     list2.add("e");
+    list2.addAll(Arrays.asList("11", "22"));
 
     // ** ArrayList **
     // Not fixed length hold only objects(Wrapper class) not primitives
@@ -53,5 +59,41 @@ public class App {
 
     System.out.println(list); // print raw element
     System.out.println(arr2); // print memory
+
+    Set<Integer> hashSet = new HashSet<>();
+    Set<Integer> treeSet = new TreeSet<>();
+
+    // Set<String> set1 = Set.of("adf", "123"); // unmutable
+
+    Boolean added = hashSet.add(1);
+    Boolean added2 = hashSet.add(1);
+    System.out.println(added);
+    System.out.println(added2);
+
+    // unordered
+    hashSet.add(2);
+    hashSet.add(44);
+    hashSet.add(11);
+    hashSet.add(223);
+
+    // ordered
+    treeSet.add(41);
+    treeSet.add(1);
+    treeSet.add(10);
+    treeSet.add(111);
+
+    // hashSet.addAll(Set.of(1, 2, 3));
+
+    // Iterator<Integer> iterator = hashSet.iterator();
+    // while (iterator.hasNext()) {
+    //   Integer next = iterator.next();
+    //   System.out.println(next); // random order
+    // }
+    // for (Integer i : treeSet) {
+    //   System.out.println(i); // random order
+    // }
+    // hashSet.forEach(i -> System.out.println(i));
+
+    System.out.println(hashSet);
   }
 }
