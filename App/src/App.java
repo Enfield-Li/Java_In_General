2b107f31570e7e9dcd42b2d45bbae53fe4386606
Enfield-Li <1076462153@qq.com>
2022-05-25 07:13:31 +0800

    2

--- App/src/App.java
+++ App/src/App.java
@@ -1,16 +1,22 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class App {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     // IntStream.range(1, 10).forEach(System.out::print);
 
     // IntStream.range(1, 10).forEach(x -> System.out.println(x));
@@ -22,12 +28,85 @@ public class App {
     //   .map(x -> Integer.parseInt(x))
     //   .sorted()
     //   .findFirst()
-    //         .ifPresent(System.out::println);
+    //   .ifPresent(System.out::println);
 
-    String[] name = { "ssssk", "kkkks", "aaaak", "kkkka" };
-    System.out.println(name);
-    
+    // String[] name = { "ssssk", "kkkks", "aaaak", "kkkka", "ads", "abc" };
+    // Arrays
+    //   .stream(name) // same as Stream.of(names)
+    //   .filter(x -> x.startsWith("a"))
+    //   .sorted()
+    //   .forEach(System.out::println);
 
+    // Arrays
+    //   .stream(new int[] { 2, 4, 6, 8, 10 })
+    //   .map(x -> x * x)
+    //   .average()
+    //   .ifPresent(System.out::println); // double
+
+    // List<String> people = Arrays.asList("aa", "bb", "cc", "ab");
+    // people
+    //   .stream()
+    //   .map(String::toLowerCase)
+    //   .filter(x -> x.startsWith("a"))
+    //   .forEach(System.out::println);
+
+    // Stream<String> bands = Files.lines(
+    //   Paths.get("D:/desktop/learn/JAVA/Java/App/src/bands.txt")
+    // );
+    // bands.sorted().filter(x -> x.length() > 13).forEach(System.out::println);
+    // //   .forEach(x -> System.out.println(x));
+    // bands.close();
+
+    // List<String> bands2 = Files
+    //   .lines(Paths.get("D:/desktop/learn/JAVA/Java/App/src/bands.txt"))
+    //   .filter(x -> x.contains("jit"))
+    //   .collect(Collectors.toList());
+    // bands2.forEach(x -> System.out.println(x));
+
+    // Stream<String> rows1 = Files.lines(
+    //   Paths.get("D:/desktop/learn/JAVA/Java/App/src/data.txt")
+    // );
+    // int rowCount = (int) rows1
+    //   .map(x -> x.split(","))
+    //   .filter(x -> x.length == 3)
+    //   .count();
+    // System.out.println(rowCount + " rows");
+    // rows1.close();
+
+    // Stream<String> rows2 = Files.lines(
+    //   Paths.get("D:/desktop/learn/JAVA/Java/App/src/data.txt")
+    // );
+    // rows2
+    //   .map(x -> x.split(","))
+    //   .filter(x -> x.length == 3)
+    //   .filter(x -> Integer.parseInt(x[1]) > 15)
+    //   .forEach(x -> System.out.println(x[0] + "  " + x[1] + "  " + x[2]));
+
+    // Stream<String> rows3 = Files.lines(
+    //   Paths.get("D:/desktop/learn/JAVA/Java/App/src/data.txt")
+    // );
+    // Map<String, Integer> map = new HashMap<>();
+    // map =
+    //   rows3
+    //     .map(x -> x.split(","))
+    //     .filter(x -> x.length == 3)
+    //     .filter(x -> Integer.parseInt(x[1]) > 15)
+    //     .collect(Collectors.toMap(x -> x[0], x -> Integer.parseInt(x[1])));
+    // rows3.close();
+    // // map.keySet().forEach(x -> System.out.println(x + "  " + map.get(x))); // Local variable map defined in an enclosing scope must be final or effectively final
+    // for (String key : map.keySet()) {
+    //   System.out.println(key + "  " + map.get(key));
+    // }
+
+    // double total = Stream
+    //   .of(7.3, 1.5, 4.8)
+    //   .reduce(0.0, (Double a, Double b) -> a + b);
+    // System.out.println(total);
+
+    // IntSummaryStatistics summary = IntStream
+    //   .of(7, 2, 19, 88, 73, 4, 10)
+    //   .summaryStatistics();
+    // System.out.println(summary);
   }
   //   private static List<Person> getPeople() {
   //     return Arrays.asList(
